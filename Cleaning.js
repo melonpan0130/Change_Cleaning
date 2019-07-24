73b5f7a46453c8bc88cf019a66ac1c4848f66e37
Eunju/kiori <eun01300714@gmail.com>
2019-07-24 16:47:55 +0900

    modified

--- Cleaning.js
+++ Cleaning.js
@@ -1,16 +1,28 @@
-window.onload = init;
-
-function init(){
-    console.log("hello");
-    document.getElementById("Form").onsubmit = Random;
-}
+var triggle = 0;
 
 function Random(){
-    var number = [];
-    for(var i=0; i<19; i++)
-        number[i] = i+1;
-
-    console.log(number);
+    var number = ['강윤수', '김나연', '김진주', '김효경', '나연경','남도희','박혜정','박희경','서지인','유란주',
+    '윤지원','이규빈','이서주', '이세하', '이유진','정은주','주미림','진세린','최민성'];
+    
+    if(triggle == 0){
+        // 겹치지 않는 18가지
+        for(var i=0; i<19; i++){
+            var random = Math.floor(Math.random()*19);
+            if(number[random] != null){
+                // 여기다 적어
+                var output = number[random] + ' ';
+                var cla = "clean"+Math.floor(i/3+1);
+                console.log(cla);
+                document.getElementById(cla).innerHTML += output;
+                console.log(number[random] + ' : ' + Math.floor(i/3+1));
+                number[random] = null;
+            }else {
+                i--;
+                continue;
+            }
+        }
+        triggle = 1;
+    }
 
     return false;
 }
